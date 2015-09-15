ad103dce6104c9d32911d19476ccf0ba0083d29d
Angie LAURENTI <lauren_a@etna-alternance.net>
2015-09-15 17:36:19 +0200

    [SDK] Change version of npm-module promise 7 to 6

--- example/index.js
+++ example/index.js
@@ -1,6 +1,7 @@
 var SDK	 		= require('../lib/index').SDK;
 var conf 		= require('./config/config.json');
-var privateKey 	= require('fs').readFileSync('./config/private_october.key');
+var privateKey 	= require('fs').readFileSync('./config/private_sept.key');
+var privateKey2 = require('fs').readFileSync('./config/private_october.key');
 
 var tokenContent = {
 	userId: 1,
@@ -8,12 +9,22 @@ var tokenContent = {
 	scopes: "toto"
 };
 
-var app = new SDK(conf);
+var app = new SDK(conf["SDK"]);
 
 var accessToken = app.signToken(tokenContent, privateKey);
+var accessToken2 = app.signToken(tokenContent, privateKey2);
 
 var testPromise = app.verifyToken(accessToken);
+var testPromise2 = app.verifyToken(accessToken2);
 
 testPromise.then( function (tokenContent) {
 	console.log(tokenContent);
+}).catch(function (err) {
+	console.log(err);;
+});
+
+testPromise2.then( function (tokenContent) {
+	console.log(tokenContent);
+}).catch(function (err) {
+	console.log(err);;
 });
