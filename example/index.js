d0d4a4760b8e655600ac3044d11728a9b646e0a1
Angie LAURENTI <lauren_a@etna-alternance.net>
2015-09-21 12:14:31 +0200

    [SDK] Add function checkAccountToken for liveapi

--- example/index.js
+++ example/index.js
@@ -4,8 +4,8 @@ var privateKey 	= require('fs').readFileSync('./config/private_sept.key');
 var privateKey2 = require('fs').readFileSync('./config/private_october.key');
 
 var tokenContent = {
-	userId: 1,
-	appId: 2,
+	account: "000002",
+	appId: "fee8a312946c4a593046ed36d2",
 	scopes: "toto"
 };
 
@@ -14,17 +14,24 @@ var app = new SDK(conf["SDK"]);
 var accessToken = app.signToken(tokenContent, privateKey);
 var accessToken2 = app.signToken(tokenContent, privateKey2);
 
-var testPromise = app.verifyToken(accessToken);
-var testPromise2 = app.verifyToken(accessToken2);
+// console.log(app.checkAccountToken(accessToken, "000003"));
+// console.log(app.checkAccountToken(accessToken, "000002"));
+console.log("Good Token:");
+console.log(accessToken);
+console.log("Bad Token :")
+console.log(accessToken2);
 
-testPromise.then( function (tokenContent) {
-	console.log(tokenContent);
-}).catch(function (err) {
-	console.log(err);;
-});
+// var testPromise = app.verifyToken(accessToken);
+// var testPromise2 = app.verifyToken(accessToken2);
 
-testPromise2.then( function (tokenContent) {
-	console.log(tokenContent);
-}).catch(function (err) {
-	console.log(err);;
-});
+// testPromise.then( function (tokenContent) {
+// 	console.log(tokenContent);
+// }).catch(function (err) {
+// 	console.log(err);;
+// });
+
+// testPromise2.then( function (tokenContent) {
+// 	console.log(tokenContent);
+// }).catch(function (err) {
+// 	console.log(err);;
+// });
