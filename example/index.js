391ee5cc97dc2563d296dfa6df514a5887e01df0
Angie LAURENTI <lauren_a@etna-alternance.net>
2015-10-19 17:45:38 +0200

    [SDK] promised function getCurrentPublicKey + verifyToken + verifyScopes

--- example/index.js
+++ example/index.js
@@ -5,31 +5,65 @@ var privateKey2 = require('fs').readFileSync('./config/private_october.key');
 var app 		= new SDK(conf["SDK"]);
 
 var tokenContent = {
-	account: "999999",
+	account: "012345",
 	appId: "fee8a312946c4a593046ed36d2",
-	scopes: ["toto::plop", "toto:*:*"]
+	scopes: ["plop::post*"]
 };
 
-// good token
-var accessToken = app.signToken(tokenContent, privateKey);
-// bad token
-var accessToken2 = app.signToken(tokenContent, privateKey2);
 
-console.log(app.verifyScopes(tokenContent.scopes, "toto::plop"));
-console.log(app.verifyScopes(tokenContent.scopes, "toto::*"));
-console.log(app.verifyScopes(tokenContent.scopes, "titi:*:*"));
+//good token
+var goodToken = app.signToken(tokenContent, privateKey);
+
+//bad token
+var badToken = app.signToken(tokenContent, privateKey2);
 
 
-var testPromise = app.verifyToken(accessToken);
-var testPromise2 = app.verifyToken(accessToken2);
+//Test on getCurrentPublicKey
+var currentPublicKey = app.getCurrentPublicKey(goodToken);
 
-testPromise.then( function (tokenContent) {
+currentPublicKey.then(function (res) {
+	console.log(res);
+}).catch(function (err) {
+	console.log(err);
+});
+
+var currentPublicKey2 = app.getCurrentPublicKey(badToken);
+
+currentPublicKey2.then(function (res) {
+	console.log(res);
+}).catch(function (err) {
+	console.log(err);
+});
+
+
+//Test verifyScopes
+verifyScopes1 = app.verifyScopes(null, "plop::postUnicorn");
+verifyScopes2 = app.verifyScopes(tokenContent.scopes, "toto::*");
+
+verifyScopes1.then(function (res) {
+	console.log(res);
+}).catch(function (err) {
+	console.log(err);
+});
+
+verifyScopes2.then(function (res) {
+	console.log(res);
+}).catch(function (err) {
+	console.log(err);
+});
+
+
+
+var verifyToken = app.verifyToken(goodToken);
+var verifyToken2 = app.verifyToken(badToken);
+
+verifyToken.then( function (tokenContent) {
 	console.log(tokenContent);
 }).catch(function (err) {
 	console.log(err);
 });
 
-testPromise2.then( function (tokenContent) {
+verifyToken2.then( function (tokenContent) {
 	console.log(tokenContent);
 }).catch(function (err) {
 	console.log(err);
